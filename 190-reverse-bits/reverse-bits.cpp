a05681a230f4141ce254357e2f316b5ac89bb3bc
Rashmi Gupta <rashmigupta.official3@gmail.com>
2025-10-15 22:09:46 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (92.31%) - LeetSync

---
+++ 190-reverse-bits/reverse-bits.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public: int reverseBits(int n) {
+        int reverse = 0;
+        for ( int i=0; i<32; i++){
+            reverse = (reverse << 1) | (n & 1); //shift left then add last bit
+            n = n >> 1; // shift n right
+        }
+        return reverse;
+    }
+};
