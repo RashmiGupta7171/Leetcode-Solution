7d4f8ee591838157e27d813a84f90ae09f563584
Rashmi Gupta <rashmigupta.official3@gmail.com>
2025-11-10 15:03:55 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (10.28%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.java
@@ -15,9 +15,11 @@ class Solution {
     private ListNode solve(ListNode node){
         if(node == null || node.next == null)
         return node;
+
         ListNode tail = node.next;
         node.next = solve (tail.next);
         tail.next = node;
+        
         return tail;
     }
 }
