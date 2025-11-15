7f55cfa83a3427af970a5ac04e38d400867bf069
Rashmi Gupta <rashmigupta.official3@gmail.com>
2025-11-15 23:02:34 +0530

    Time: 11 ms (20.39%) | Memory: 52.4 MB (5.24%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> seen = new HashSet<>();
+
+        for(int i =0 ; i < 9 ; i++){
+
+            for(int j = 0 ; j < 9 ; j++){
+
+                char current = board[i][j];
+
+                if(current != '.'){
+
+                  if(!seen.add(current+ "int row" + i))
+                  return false;
+
+                  if(!seen.add(current + "int column" + j))
+                  return false;
+
+                  if(!seen.add(current + "int box" + i/3 + "-" + j/3))
+                  return false;
+                }
+            }
+        }
+        return true;
+    }
+}
