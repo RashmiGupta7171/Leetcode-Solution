d4ac082b8b8cacb09a210eb357cefc3dc410de29
Rashmi Gupta <rashmigupta.official3@gmail.com>
2025-11-16 21:39:14 +0530

    Time: 2 ms (93.97%) | Memory: 43.1 MB (24.68%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,34 +1,55 @@
 
-        class Solution {
-    public String countAndSay(int n) {
-        if (n == 1) return "1";
+//         class Solution {
+//     public String countAndSay(int n) {
+//         if (n == 1) return "1";
         
-        String res = "1";
-        for (int i = 2; i <= n; i++) {
-            res = getNext(res);
-        }
-        return res;
-    }
+//         String res = "1";
+//         for (int i = 2; i <= n; i++) {
+//             res = getNext(res);
+//         }
+//         return res;
+//     }
 
-    private String getNext(String s) {
-        StringBuilder sb = new StringBuilder();
-        int count = 1;  // count of current character
+//     private String getNext(String s) {
+//         StringBuilder sb = new StringBuilder();
+//         int count = 1;  // count of current character
         
-        for (int i = 1; i < s.length(); i++) {
-            // If current char matches previous one, increase count
-            if (s.charAt(i) == s.charAt(i - 1)) {
+//         for (int i = 1; i < s.length(); i++) {
+//             // If current char matches previous one, increase count
+//             if (s.charAt(i) == s.charAt(i - 1)) {
+//                 count++;
+//             } else {
+//                 // Otherwise, append count and previous char
+//                 sb.append(count).append(s.charAt(i - 1));
+//                 count = 1; // reset count
+//             }
+//         }
+        
+//         // Append the last run
+//         sb.append(count).append(s.charAt(s.length() - 1));
+        
+//         return sb.toString();
+//     }
+// }
+
+class Solution{
+    public String countAndSay(int n){
+        if(n ==1)
+        return "1";
+
+        String say = countAndSay(n - 1);
+        StringBuilder result = new StringBuilder();
+
+        for(int i = 0; i < say.length() ; i++){
+            char ch = say.charAt(i);
+            int count = 1;
+
+            while(i < say.length() - 1 && say.charAt(i) == say.charAt(i+1)){
                 count++;
-            } else {
-                // Otherwise, append count and previous char
-                sb.append(count).append(s.charAt(i - 1));
-                count = 1; // reset count
+                i++;
             }
+            result.append(count).append(ch);
         }
-        
-        // Append the last run
-        sb.append(count).append(s.charAt(s.length() - 1));
-        
-        return sb.toString();
+        return result.toString();
     }
 }
-
