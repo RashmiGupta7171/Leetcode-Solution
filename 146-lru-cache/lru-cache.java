e38e02f77ea0e343c82441d3a6c41e7e1f84a954
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-01-20 22:09:50 +0530

    Time: 44 ms (83.18%) | Memory: 131.1 MB (70.86%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,101 @@
+// class LRUCache {
+
+//     public LRUCache(int capacity) {
+        
+//     }
+    
+//     public int get(int key) {
+        
+//     }
+    
+//     public void put(int key, int value) {
+        
+//     }
+// }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
+ import java.util.HashMap;
+import java.util.Map;
+
+class LRUCache {
+    private class Node {
+        int key, value;
+        Node prev, next;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> map;
+    private final Node head, tail;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.map = new HashMap<>();
+
+        // Dummy head and tail to avoid null checks
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        Node node = map.get(key);
+        if (node == null) return -1;
+
+        moveToHead(node);
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        Node node = map.get(key);
+
+        if (node != null) {
+            node.value = value;
+            moveToHead(node);
+        } else {
+            Node newNode = new Node(key, value);
+            map.put(key, newNode);
+            addToHead(newNode);
+
+            if (map.size() > capacity) {
+                Node lru = removeTail();
+                map.remove(lru.key);
+            }
+        }
+    }
+
+    // Helper Methods
+    private void addToHead(Node node) {
+        node.prev = head;
+        node.next = head.next;
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(Node node) {
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void moveToHead(Node node) {
+        removeNode(node);
+        addToHead(node);
+    }
+
+    private Node removeTail() {
+        Node lru = tail.prev;
+        removeNode(lru);
+        return lru;
+    }
+}
