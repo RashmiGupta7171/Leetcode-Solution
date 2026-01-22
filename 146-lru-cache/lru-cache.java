1c7fef4afc634f7ac51888f6fe5ea2cf4987e825
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-01-23 01:15:51 +0530

    Time: 53 ms (19.51%) | Memory: 130.7 MB (84.69%) - LeetSync

--- 146-lru-cache/lru-cache.java
+++ 146-lru-cache/lru-cache.java
@@ -1,101 +1,63 @@
-// class LRUCache {
-
-//     public LRUCache(int capacity) {
-        
-//     }
-    
-//     public int get(int key) {
-        
-//     }
-    
-//     public void put(int key, int value) {
-        
-//     }
-// }
-
 /**
  * Your LRUCache object will be instantiated and called as such:
  * LRUCache obj = new LRUCache(capacity);
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
- import java.util.HashMap;
-import java.util.Map;
-
 class LRUCache {
-    private class Node {
-        int key, value;
-        Node prev, next;
 
-        Node(int key, int value) {
-            this.key = key;
-            this.value = value;
-        }
-    }
-
-    private final int capacity;
-    private final Map<Integer, Node> map;
-    private final Node head, tail;
+    Node head = new Node(0, 0), tail = new Node(0, 0);
+    Map<Integer, Node> map = new HashMap<>();
+    int capacity;
 
     public LRUCache(int capacity) {
         this.capacity = capacity;
-        this.map = new HashMap<>();
-
-        // Dummy head and tail to avoid null checks
-        head = new Node(0, 0);
-        tail = new Node(0, 0);
-
         head.next = tail;
         tail.prev = head;
     }
 
     public int get(int key) {
-        Node node = map.get(key);
-        if (node == null) return -1;
-
-        moveToHead(node);
-        return node.value;
+        if (map.containsKey(key)) {
+            Node data = map.get(key);
+            remove(data);
+            insert(data);
+            return data.value;
+        }
+        return -1;
     }
 
     public void put(int key, int value) {
-        Node node = map.get(key);
-
-        if (node != null) {
-            node.value = value;
-            moveToHead(node);
-        } else {
-            Node newNode = new Node(key, value);
-            map.put(key, newNode);
-            addToHead(newNode);
-
-            if (map.size() > capacity) {
-                Node lru = removeTail();
-                map.remove(lru.key);
-            }
+        if (map.containsKey(key)) {
+            remove(map.get(key));
         }
+        if (capacity == map.size()) {
+            remove(tail.prev); // remove LRU
+        }
+        insert(new Node(key, value));
     }
 
-    // Helper Methods
-    private void addToHead(Node node) {
-        node.prev = head;
-        node.next = head.next;
-        head.next.prev = node;
-        head.next = node;
-    }
-
-    private void removeNode(Node node) {
+    void remove(Node node) {
+        map.remove(node.key);
         node.prev.next = node.next;
         node.next.prev = node.prev;
     }
 
-    private void moveToHead(Node node) {
-        removeNode(node);
-        addToHead(node);
+    void insert(Node node) {
+        map.put(node.key, node);
+        Node headNext = head.next;
+        head.next = node;
+        node.prev = head;
+        node.next = headNext;
+        headNext.prev = node;
     }
 
-    private Node removeTail() {
-        Node lru = tail.prev;
-        removeNode(lru);
-        return lru;
+    class Node {
+        Node prev, next;
+        int key, value;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
