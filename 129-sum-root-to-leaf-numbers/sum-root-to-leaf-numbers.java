85033bcc79ac9e305bc93eec7a572d5a154b0992
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-01-22 23:24:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (10.08%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -13,29 +13,6 @@
  *     }
  * }
  */
-//  class Solution {
-
-//     public int sumNumbers(TreeNode root) {
-//         return dfs(root, 0);
-//     }
-
-//     private int dfs(TreeNode node, int current) {
-//         if (node == null) {
-//             return 0;
-//         }
-
-//         // Build the number along the path
-//         current = current * 10 + node.val;
-
-//         // If it's a leaf, return the number formed
-//         if (node.left == null && node.right == null) {
-//             return current;
-//         }
-
-//         // Recurse for left and right subtrees
-//         return dfs(node.left, current) + dfs(node.right, current);
-//     }
-// }
 class Solution{
     int sum = 0;
     public void sumNum(TreeNode node , int num){
