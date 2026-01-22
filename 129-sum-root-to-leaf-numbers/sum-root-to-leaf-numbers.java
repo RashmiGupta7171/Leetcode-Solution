4e71398a379aac94c98c46773c9cc22c64f66d14
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-01-22 23:24:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (10.08%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -13,26 +13,44 @@
  *     }
  * }
  */
- class Solution {
+//  class Solution {
 
-    public int sumNumbers(TreeNode root) {
-        return dfs(root, 0);
+//     public int sumNumbers(TreeNode root) {
+//         return dfs(root, 0);
+//     }
+
+//     private int dfs(TreeNode node, int current) {
+//         if (node == null) {
+//             return 0;
+//         }
+
+//         // Build the number along the path
+//         current = current * 10 + node.val;
+
+//         // If it's a leaf, return the number formed
+//         if (node.left == null && node.right == null) {
+//             return current;
+//         }
+
+//         // Recurse for left and right subtrees
+//         return dfs(node.left, current) + dfs(node.right, current);
+//     }
+// }
+class Solution{
+    int sum = 0;
+    public void sumNum(TreeNode node , int num){
+        num = num*10 + node.val;
+        if(node.left == null && node.right == null){
+            sum = sum + num;
+            return ;
+        }
+        if(node.left != null)
+        sumNum(node.left , num);
+        if(node.right != null)
+        sumNum(node.right , num);
     }
-
-    private int dfs(TreeNode node, int current) {
-        if (node == null) {
-            return 0;
-        }
-
-        // Build the number along the path
-        current = current * 10 + node.val;
-
-        // If it's a leaf, return the number formed
-        if (node.left == null && node.right == null) {
-            return current;
-        }
-
-        // Recurse for left and right subtrees
-        return dfs(node.left, current) + dfs(node.right, current);
+    public int sumNumbers(TreeNode root){
+        sumNum(root , 0);
+        return sum;
     }
 }
