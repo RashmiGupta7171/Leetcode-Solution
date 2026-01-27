f77058ed577c586c9c64d689f602ff48d1ff93b0
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-01-27 21:44:43 +0530

    Time: 4 ms (62.04%) | Memory: 45.5 MB (40.01%) - LeetSync

--- 137-single-number-ii/single-number-ii.java
+++ 137-single-number-ii/single-number-ii.java
@@ -3,7 +3,6 @@ class Solution {
     public int singleNumber(int[] nums) {
         int result = 0;
 
-        // Check each bit position
         for (int i = 0; i < 32; i++) {
             int count = 0;
 
@@ -12,8 +11,6 @@ class Solution {
                     count++;
                 }
             }
-
-            // If count is not multiple of 3, set this bit
             if (count % 3 != 0) {
                 result |= (1 << i);
             }
