ff96f7bc4d7871c22fe20e2db3805b992b6e8310
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-01-28 21:41:12 +0530

    Time: 25 ms (75.15%) | Memory: 44.4 MB (30.93%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -18,47 +18,17 @@ class Node {
 }
 */
 
-
-// import java.util.*;
-
-// // Definition for a Node.
-// class Node {
-//     public int val;
-//     public List<Node> neighbors;
-
-//     public Node() {
-//         val = 0;
-//         neighbors = new ArrayList<Node>();
-//     }
-
-//     public Node(int _val) {
-//         val = _val;
-//         neighbors = new ArrayList<Node>();
-//     }
-
-//     public Node(int _val, ArrayList<Node> _neighbors) {
-//         val = _val;
-//         neighbors = _neighbors;
-//     }
-// }
-
 class Solution {
 
     private Map<Node, Node> map = new HashMap<>();
 
     public Node cloneGraph(Node node) {
         if (node == null) return null;
-
-        // If node already cloned, return it
         if (map.containsKey(node)) {
             return map.get(node);
         }
-
-        // Create clone of current node
         Node clone = new Node(node.val);
         map.put(node, clone);
-
-        // Clone all neighbors
         for (Node neighbor : node.neighbors) {
             clone.neighbors.add(cloneGraph(neighbor));
         }
@@ -66,4 +36,26 @@ class Solution {
         return clone;
     }
 }
+
+// class Solution{
+//     public Node cloneGraph(Node node){
+//         if(node == null)
+//         return null;
+//         Map<Node , Node> map = new HashMap<>();
+//         Queue<Node> queue = new ArrayDeque<>();
+//         queue.add(node);
+//         map.put(node , new Node(node.val , new ArrayList<>()));
+//         while(!queue.isEmpty()){
+//             Node h = queue.poll();
+//             for(Node neighbor : h.neighbors){
+//                 if(!map.containsKey(neighbor)){
+//                     map.put(neighbor , new Node(neighbor.val , new ArrayList<>()));
+//                 }
+//                 map.get(h).neighbors.add(map.get(neighbor));
+//                 queue.add(neighbor);
+//             }
+//         }
+//         return map.get(node);
+//     }
+// }
 
