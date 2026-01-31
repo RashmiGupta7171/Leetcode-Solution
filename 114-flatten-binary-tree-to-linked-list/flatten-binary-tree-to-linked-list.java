625ad3a0c05c12fca767523cec02d3918713e111
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-01-31 23:02:07 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (44.55%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.java
@@ -13,24 +13,44 @@
  *     }
  * }
  */
-class Solution {
+// class Solution {
 
-    public void flatten(TreeNode root) {
-        if (root == null) return;
+//     public void flatten(TreeNode root) {
+//         if (root == null) return;
 
-        flatten(root.left);
-        flatten(root.right);
+//         flatten(root.left);
+//         flatten(root.right);
 
-        TreeNode rightSubtree = root.right;
+//         TreeNode rightSubtree = root.right;
 
-        root.right = root.left;
-        root.left = null;
+//         root.right = root.left;
+//         root.left = null;
 
-        TreeNode curr = root;
-        while (curr.right != null) {
-            curr = curr.right;
+//         TreeNode curr = root;
+//         while (curr.right != null) {
+//             curr = curr.right;
+//         }
+
+//         curr.right = rightSubtree;
+//     }
+// }
+
+class Solution{
+    public void flatten(TreeNode root){
+        if(root ==null)
+        return;
+        while(root != null){
+            if(root.left != null){
+                TreeNode left = root.left;
+                TreeNode current = left;
+
+                while(current.right != null)
+                current = current.right;
+                current.right = root.right;
+                root.left = null;
+                root.right = left;
+            }
+            root = root.right;
         }
-
-        curr.right = rightSubtree;
     }
 }
