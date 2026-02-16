d81f80bd32a11d086dd57d60f0201b4e70d5f5bb
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-02-16 21:56:15 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (35.50%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -7,14 +7,12 @@ class Solution {
             int mid = left + (right - left) / 2;
 
             if (nums[mid] > nums[mid + 1]) {
-                // Peak is in the left part including mid
                 right = mid;
             } else {
-                // Peak is in the right part
                 left = mid + 1;
             }
         }
 
-        return left; // or right (both are same here)
+        return left; 
     }
 }
