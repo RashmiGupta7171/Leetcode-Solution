628bbc855ddc3b9a4b92bd0bb8f2ad82d636e69b
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-02-24 00:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (67.57%) - LeetSync

---
+++ 899-binary-gap/binary-gap.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int binaryGap(int n) {
+        int lastPos = -1;
+        int maxDistance = 0;
+        int position = 0;
+
+        while (n > 0) {
+            if ((n & 1) == 1) {  // current bit is 1
+                if (lastPos != -1) {
+                    maxDistance = Math.max(maxDistance, position - lastPos);
+                }
+                lastPos = position;
+            }
+            n >>= 1;
+            position++;
+        }
+
+        return maxDistance;
+    }
+}
