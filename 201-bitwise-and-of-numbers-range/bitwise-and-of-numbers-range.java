aabb8597b74fdbea778502e53840bdd0ce3d1208
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-03-10 21:30:42 +0530

    Time: 3 ms (100.00%) | Memory: 46.1 MB (34.83%) - LeetSync

--- 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
+++ 201-bitwise-and-of-numbers-range/bitwise-and-of-numbers-range.java
@@ -2,14 +2,11 @@ class Solution {
     public int rangeBitwiseAnd(int left, int right) {
         int shift = 0;
         
-        // Find common prefix
         while (left < right) {
             left >>= 1;
             right >>= 1;
             shift++;
         }
-        
-        // Restore shifted bits
         return left << shift;
     }
 }
