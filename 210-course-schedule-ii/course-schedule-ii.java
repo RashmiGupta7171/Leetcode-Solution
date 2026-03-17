8cf20fbe4ef91ce4c07f715e13e1ad2d11e71f64
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-03-17 21:41:29 +0530

    Time: 5 ms (82.23%) | Memory: 46.7 MB (79.53%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,24 +1,19 @@
-
-
 class Solution {
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        // Step 1: Create graph
-        List<List<Integer>> graph = new ArrayList<>();
+           List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < numCourses; i++) {
             graph.add(new ArrayList<>());
         }
 
         int[] indegree = new int[numCourses];
 
-        // Step 2: Build graph & indegree
-        for (int[] pre : prerequisites) {
+            for (int[] pre : prerequisites) {
             int course = pre[0];
             int prereq = pre[1];
             graph.get(prereq).add(course);
             indegree[course]++;
         }
 
-        // Step 3: Add nodes with indegree 0
         Queue<Integer> queue = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
             if (indegree[i] == 0) {
@@ -28,8 +23,6 @@ class Solution {
 
         int[] result = new int[numCourses];
         int index = 0;
-
-        // Step 4: BFS
         while (!queue.isEmpty()) {
             int curr = queue.poll();
             result[index++] = curr;
@@ -42,11 +35,10 @@ class Solution {
             }
         }
 
-        // Step 5: Check if valid
         if (index == numCourses) {
             return result;
         }
 
-        return new int[0]; // cycle detected
+        return new int[0]; 
     }
 }
