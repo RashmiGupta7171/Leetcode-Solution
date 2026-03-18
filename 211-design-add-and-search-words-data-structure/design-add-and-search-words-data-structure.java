e863d03f2fc6569ab43ffb4b618d9f2f7e5b8554
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-03-18 18:03:24 +0530

    Time: 201 ms (47.39%) | Memory: 269.8 MB (97.21%) - LeetSync

---
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -0,0 +1,61 @@
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
+ class WordDictionary {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd;
+    }
+
+    private TrieNode root;
+
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    // Add word into Trie
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            int index = ch - 'a';
+            if (node.children[index] == null) {
+                node.children[index] = new TrieNode();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+    
+    // Search with support for '.'
+    public boolean search(String word) {
+        return dfs(word, 0, root);
+    }
+    
+    private boolean dfs(String word, int index, TrieNode node) {
+        if (node == null) return false;
+        
+        if (index == word.length()) {
+            return node.isEnd;
+        }
+        
+        char ch = word.charAt(index);
+        
+        if (ch == '.') {
+            // Try all possible paths
+            for (TrieNode child : node.children) {
+                if (child != null && dfs(word, index + 1, child)) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            // Normal character
+            return dfs(word, index + 1, node.children[ch - 'a']);
+        }
+    }
+}
