6f6b89672b4bc03326be409cd14e0cb7581ec91c
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-03-22 22:11:32 +0530

    Time: 21 ms (55.22%) | Memory: 51.1 MB (98.50%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -0,0 +1,42 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        List<String> result = new ArrayList<>();
+        if (s.length() < 10) return result;
+
+        // Map characters to 2-bit values
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 0);
+        map.put('C', 1);
+        map.put('G', 2);
+        map.put('T', 3);
+
+        Set<Integer> seen = new HashSet<>();
+        Set<Integer> added = new HashSet<>();
+
+        int hash = 0;
+
+        // Build first window
+        for (int i = 0; i < 10; i++) {
+            hash = (hash << 2) | map.get(s.charAt(i));
+        }
+        seen.add(hash);
+
+        int mask = (1 << 20) - 1; // Keep only last 20 bits
+
+        // Slide window
+        for (int i = 10; i < s.length(); i++) {
+            hash = ((hash << 2) | map.get(s.charAt(i))) & mask;
+
+            if (seen.contains(hash)) {
+                if (!added.contains(hash)) {
+                    result.add(s.substring(i - 9, i + 1));
+                    added.add(hash);
+                }
+            } else {
+                seen.add(hash);
+            }
+        }
+
+        return result;
+    }
+}
