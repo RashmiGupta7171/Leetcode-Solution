f4ee6bd04a6419533ed0a8fb860d2906ccf7ee5e
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-04-15 14:39:49 +0530

    Time: 2 ms (85.89%) | Memory: 43.3 MB (90.88%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,28 +1,24 @@
-
-
 class Solution {
     public List<Integer> diffWaysToCompute(String expression) {
         return helper(expression, new HashMap<>());
     }
 
     private List<Integer> helper(String exp, Map<String, List<Integer>> memo) {
-        // If already computed, return from memo
         if (memo.containsKey(exp)) {
             return memo.get(exp);
         }
 
         List<Integer> result = new ArrayList<>();
 
-        // Try splitting at each operator
         for (int i = 0; i < exp.length(); i++) {
             char ch = exp.charAt(i);
 
             if (ch == '+' || ch == '-' || ch == '*') {
-                // Divide into left and right
+           
                 List<Integer> left = helper(exp.substring(0, i), memo);
                 List<Integer> right = helper(exp.substring(i + 1), memo);
 
-                // Combine results
+        
                 for (int l : left) {
                     for (int r : right) {
                         if (ch == '+') {
