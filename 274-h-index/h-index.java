193e4253f955a3c0ac0dc5e10acbce0d7d5282b6
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-04-25 22:25:58 +0530

    Time: 5 ms (68.68%) | Memory: 43.9 MB (7.08%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -2,10 +2,8 @@ class Solution {
     public int hIndex(int[] citations) {
         int n = citations.length;
         
-        // Sort the array
         Arrays.sort(citations);
-        
-        // Traverse and find h-index
+     
         for (int i = 0; i < n; i++) {
             int h = n - i;
             if (citations[i] >= h) {
