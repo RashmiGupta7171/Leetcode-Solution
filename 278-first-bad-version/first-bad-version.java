2b5989210f72917d655286d5d17f3d9f3d6ba432
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-04-27 00:08:35 +0530

    Time: 13 ms (99.88%) | Memory: 42.4 MB (5.81%) - LeetSync

--- 278-first-bad-version/first-bad-version.java
+++ 278-first-bad-version/first-bad-version.java
@@ -2,7 +2,6 @@
       boolean isBadVersion(int version); */
 
 public class Solution extends VersionControl {
-    
     public int firstBadVersion(int n) {
         int left = 1;
         int right = n;
@@ -11,12 +10,12 @@ public class Solution extends VersionControl {
             int mid = left + (right - left) / 2;
             
             if (isBadVersion(mid)) {
-                right = mid; // mid might be the first bad
+                right = mid; 
             } else {
-                left = mid + 1; // first bad is after mid
+                left = mid + 1; 
             }
         }
         
-        return left; // or right (both are same here)
+        return left; 
     }
 }
