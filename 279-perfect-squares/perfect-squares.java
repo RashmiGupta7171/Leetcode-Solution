da741f72d12d4d2e6d2aa0dc576a211b1a18ea59
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-04-28 17:23:37 +0530

    Time: 30 ms (76.57%) | Memory: 44.4 MB (75.44%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -2,12 +2,10 @@ class Solution {
     public int numSquares(int n) {
         int[] dp = new int[n + 1];
 
-        // Base case
         dp[0] = 0;
 
         for (int i = 1; i <= n; i++) {
-            dp[i] = i; // maximum (1^2 + 1^2 + ...)
-
+            dp[i] = i; 
             for (int j = 1; j * j <= i; j++) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
