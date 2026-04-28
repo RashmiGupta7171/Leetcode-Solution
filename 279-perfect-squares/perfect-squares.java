fb3716287479ac941c60831ff578f0947947b975
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-04-28 17:22:04 +0530

    Time: 31 ms (70.38%) | Memory: 44.5 MB (64.55%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+
+        // Base case
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i; // maximum (1^2 + 1^2 + ...)
+
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
