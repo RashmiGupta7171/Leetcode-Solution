c42fdb1c032106d61a3697d57978fe1755f33a1e
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-04-30 14:58:47 +0530

    Time: 4 ms (83.55%) | Memory: 43.8 MB (11.26%) - LeetSync

---
+++ 284-peeking-iterator/peeking-iterator.java
@@ -0,0 +1,43 @@
+// Java Iterator interface reference:
+// https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
+
+
+
+class PeekingIterator implements Iterator<Integer> {
+    
+    private Iterator<Integer> iterator;
+    private Integer nextElement;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        this.iterator = iterator;
+        // Initialize nextElement
+        if (iterator.hasNext()) {
+            nextElement = iterator.next();
+        }
+    }
+
+    // Returns the next element without moving the pointer
+    public Integer peek() {
+        return nextElement;
+    }
+
+    // Returns the next element and moves pointer forward
+    @Override
+    public Integer next() {
+        Integer result = nextElement;
+        
+        if (iterator.hasNext()) {
+            nextElement = iterator.next();
+        } else {
+            nextElement = null;
+        }
+        
+        return result;
+    }
+
+    // Returns true if there are more elements
+    @Override
+    public boolean hasNext() {
+        return nextElement != null;
+    }
+}
