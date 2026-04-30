a621881d4535e2f9cf33b76412cd220e1f3c85a1
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-04-30 14:59:15 +0530

    Time: 4 ms (83.55%) | Memory: 43.1 MB (97.90%) - LeetSync

--- 284-peeking-iterator/peeking-iterator.java
+++ 284-peeking-iterator/peeking-iterator.java
@@ -1,8 +1,5 @@
 // Java Iterator interface reference:
 // https://docs.oracle.com/javase/8/docs/api/java/util/Iterator.html
-
-
-
 class PeekingIterator implements Iterator<Integer> {
     
     private Iterator<Integer> iterator;
