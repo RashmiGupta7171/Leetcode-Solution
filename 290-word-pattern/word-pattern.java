8f5c2dbaed02aa1096751d83d2b8e90369d7ccdf
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-05-03 21:25:00 +0530

    Time: 1 ms (83.18%) | Memory: 43 MB (17.99%) - LeetSync

--- 290-word-pattern/word-pattern.java
+++ 290-word-pattern/word-pattern.java
@@ -1,22 +1,18 @@
 class Solution {
     public boolean wordPattern(String pattern, String s) {
         String[] words = s.split(" ");
-        
-        // Step 1: Length check
+ 
         if (pattern.length() != words.length) {
             return false;
         }
-        
-        // Step 2: Create mappings
+       
         Map<Character, String> charToWord = new HashMap<>();
         Map<String, Character> wordToChar = new HashMap<>();
-        
-        // Step 3: Iterate and check bijection
+
         for (int i = 0; i < pattern.length(); i++) {
             char c = pattern.charAt(i);
             String word = words[i];
             
-            // Check char -> word mapping
             if (charToWord.containsKey(c)) {
                 if (!charToWord.get(c).equals(word)) {
                     return false;
@@ -25,7 +21,6 @@ class Solution {
                 charToWord.put(c, word);
             }
             
-            // Check word -> char mapping
             if (wordToChar.containsKey(word)) {
                 if (wordToChar.get(word) != c) {
                     return false;
