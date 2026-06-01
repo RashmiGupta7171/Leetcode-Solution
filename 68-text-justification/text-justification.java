d480543da552908505549097794376da6248469d
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-06-01 22:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (62.29%) - LeetSync

---
+++ 68-text-justification/text-justification.java
@@ -0,0 +1,67 @@
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> result = new ArrayList<>();
+        int i = 0;
+
+        while (i < words.length) {
+            int lineLength = words[i].length();
+            int j = i + 1;
+
+            // Find how many words can fit in the current line
+            while (j < words.length &&
+                   lineLength + 1 + words[j].length() <= maxWidth) {
+                lineLength += 1 + words[j].length();
+                j++;
+            }
+
+            int numWords = j - i;
+            StringBuilder line = new StringBuilder();
+
+            // Last line or line with only one word
+            if (j == words.length || numWords == 1) {
+                for (int k = i; k < j; k++) {
+                    line.append(words[k]);
+                    if (k < j - 1) {
+                        line.append(" ");
+                    }
+                }
+
+                while (line.length() < maxWidth) {
+                    line.append(" ");
+                }
+            } else {
+                int totalChars = 0;
+                for (int k = i; k < j; k++) {
+                    totalChars += words[k].length();
+                }
+
+                int totalSpaces = maxWidth - totalChars;
+                int gaps = numWords - 1;
+
+                int spacesPerGap = totalSpaces / gaps;
+                int extraSpaces = totalSpaces % gaps;
+
+                for (int k = i; k < j; k++) {
+                    line.append(words[k]);
+
+                    if (k < j - 1) {
+                        int spaces = spacesPerGap;
+                        if (extraSpaces > 0) {
+                            spaces++;
+                            extraSpaces--;
+                        }
+
+                        for (int s = 0; s < spaces; s++) {
+                            line.append(" ");
+                        }
+                    }
+                }
+            }
+
+            result.add(line.toString());
+            i = j;
+        }
+
+        return result;
+    }
+}
