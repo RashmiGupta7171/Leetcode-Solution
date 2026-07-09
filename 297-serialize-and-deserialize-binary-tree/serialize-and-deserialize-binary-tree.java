81fac2736e607cbac8483df6727c217dbe88e174
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-07-09 22:53:49 +0530

    Time: 9 ms (83.03%) | Memory: 48.9 MB (8.34%) - LeetSync

---
+++ 297-serialize-and-deserialize-binary-tree/serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,64 @@
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        StringBuilder sb = new StringBuilder();
+        preorder(root, sb);
+        return sb.toString();
+    }
+
+    private void preorder(TreeNode node, StringBuilder sb) {
+        if (node == null) {
+            sb.append("null,");
+            return;
+        }
+
+        sb.append(node.val).append(",");
+        preorder(node.left, sb);
+        preorder(node.right, sb);
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        String[] arr = data.split(",");
+        Queue<String> queue = new LinkedList<>();
+
+        for (String s : arr) {
+            queue.offer(s);
+        }
+
+        return buildTree(queue);
+    }
+
+    private TreeNode buildTree(Queue<String> queue) {
+        String val = queue.poll();
+
+        if (val.equals("null")) {
+            return null;
+        }
+
+        TreeNode node = new TreeNode(Integer.parseInt(val));
+        node.left = buildTree(queue);
+        node.right = buildTree(queue);
+
+        return node;
+    }
+}
+
+/**
+ * Your Codec object will be instantiated and called as such:
+ * Codec ser = new Codec();
+ * Codec deser = new Codec();
+ * TreeNode ans = deser.deserialize(ser.serialize(root));
+ */
