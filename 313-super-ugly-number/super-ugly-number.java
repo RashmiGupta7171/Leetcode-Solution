1b61c34f33423fe41475ac8de886817c5cba7774
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-07-12 22:05:07 +0530

    Time: 44 ms (52.17%) | Memory: 43.4 MB (98.68%) - LeetSync

---
+++ 313-super-ugly-number/super-ugly-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        int k = primes.length;
+
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        int[] index = new int[k];
+
+        for (int i = 1; i < n; i++) {
+            long next = Long.MAX_VALUE;
+
+            // Find next minimum candidate
+            for (int j = 0; j < k; j++) {
+                next = Math.min(next, (long) dp[index[j]] * primes[j]);
+            }
+
+            dp[i] = (int) next;
+
+            // Increment all pointers that produced the minimum
+            for (int j = 0; j < k; j++) {
+                if ((long) dp[index[j]] * primes[j] == next) {
+                    index[j]++;
+                }
+            }
+        }
+
+        return dp[n - 1];
+    }
+}
