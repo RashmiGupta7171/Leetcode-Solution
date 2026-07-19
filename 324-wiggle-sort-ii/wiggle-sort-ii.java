157cefd48b715070b5ecacf0af6b6302ebc1d487
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-07-19 21:51:47 +0530

    Time: 9 ms (61.35%) | Memory: 48.7 MB (79.49%) - LeetSync

---
+++ 324-wiggle-sort-ii/wiggle-sort-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void wiggleSort(int[] nums) {
+        int n = nums.length;
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
+
+        int left = (n + 1) / 2 - 1; // End of first half
+        int right = n - 1;          // End of second half
+
+        for (int i = 0; i < n; i++) {
+            if ((i & 1) == 0) {
+                nums[i] = sorted[left--];
+            } else {
+                nums[i] = sorted[right--];
+            }
+        }
+    }
+}
