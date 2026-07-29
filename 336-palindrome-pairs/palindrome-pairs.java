1c4b5cf5776114d1b3fadd3f1351cbe056ad7e7e
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-07-29 16:39:53 +0530

    Time: 1349 ms (46.35%) | Memory: 60.7 MB (74.73%) - LeetSync

---
+++ 336-palindrome-pairs/palindrome-pairs.java
@@ -0,0 +1,54 @@
+
+
+class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> result = new ArrayList<>();
+        Map<String, Integer> map = new HashMap<>();
+
+        // Store reversed lookup
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i], i);
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            String word = words[i];
+            int len = word.length();
+
+            for (int j = 0; j <= len; j++) {
+                String prefix = word.substring(0, j);
+                String suffix = word.substring(j);
+
+                // Case 1: Prefix is palindrome
+                if (isPalindrome(prefix)) {
+                    String revSuffix = new StringBuilder(suffix).reverse().toString();
+                    Integer idx = map.get(revSuffix);
+                    if (idx != null && idx != i) {
+                        result.add(Arrays.asList(idx, i));
+                    }
+                }
+
+                // Case 2: Suffix is palindrome
+                // j != len avoids duplicates
+                if (j != len && isPalindrome(suffix)) {
+                    String revPrefix = new StringBuilder(prefix).reverse().toString();
+                    Integer idx = map.get(revPrefix);
+                    if (idx != null && idx != i) {
+                        result.add(Arrays.asList(i, idx));
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left++) != s.charAt(right--)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
