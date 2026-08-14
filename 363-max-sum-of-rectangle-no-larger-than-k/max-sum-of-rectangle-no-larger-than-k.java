425e552ca8b73b35c2934d3cc5ace51ada614708
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-08-15 01:03:41 +0530

    Time: 231 ms (72.75%) | Memory: 47 MB (70.44%) - LeetSync

--- 363-max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.java
+++ 363-max-sum-of-rectangle-no-larger-than-k/max-sum-of-rectangle-no-larger-than-k.java
@@ -1,4 +1,3 @@
-
 class Solution {
     public int maxSumSubmatrix(int[][] matrix, int k) {
         int m = matrix.length;
@@ -6,20 +5,16 @@ class Solution {
 
         int maxSum = Integer.MIN_VALUE;
 
-        // Fix left column
         for (int left = 0; left < n; left++) {
 
             int[] rowSum = new int[m];
 
-            // Fix right column
             for (int right = left; right < n; right++) {
 
-                // Add current column to rowSum
                 for (int row = 0; row < m; row++) {
                     rowSum[row] += matrix[row][right];
                 }
 
-                // Find max subarray sum <= k
                 TreeSet<Integer> set = new TreeSet<>();
                 set.add(0);
 
@@ -28,7 +23,6 @@ class Solution {
                 for (int sum : rowSum) {
                     prefixSum += sum;
 
-                    // Need previous prefix >= prefixSum - k
                     Integer target = set.ceiling(prefixSum - k);
 
                     if (target != null) {
@@ -38,7 +32,6 @@ class Solution {
                     set.add(prefixSum);
                 }
 
-                // If we found k, this is the best possible answer
                 if (maxSum == k) {
                     return k;
                 }
