cdf6d516a03895e40ba688e182bcfc0d2aabac36
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-08-31 19:13:40 +0530

    Time: 52 ms (53.49%) | Memory: 52.1 MB (82.89%) - LeetSync

---
+++ 384-shuffle-an-array/shuffle-an-array.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+class Solution {
+
+    private int[] original;
+    private int[] nums;
+    private Random random;
+
+    public Solution(int[] nums) {
+        this.original = nums.clone();
+        this.nums = nums.clone();
+        this.random = new Random();
+    }
+
+    public int[] reset() {
+        nums = original.clone();
+        return nums;
+    }
+
+    public int[] shuffle() {
+        for (int i = nums.length - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+
+            // Swap nums[i] and nums[j]
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+
+        return nums;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
