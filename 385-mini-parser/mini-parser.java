34b6b7f71c32a14c0cbc9add18957cdcdda624e8
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-09-01 22:15:14 +0530

    Time: 2 ms (99.56%) | Memory: 47.8 MB (18.02%) - LeetSync

---
+++ 385-mini-parser/mini-parser.java
@@ -0,0 +1,104 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *     // Constructor initializes an empty nested list.
+ *     public NestedInteger();
+ *
+ *     // Constructor initializes a single integer.
+ *     public NestedInteger(int value);
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // Set this NestedInteger to hold a single integer.
+ *     public void setInteger(int value);
+ *
+ *     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
+ *     public void add(NestedInteger ni);
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return empty list if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+ public class Solution {
+
+    public NestedInteger deserialize(String s) {
+
+        // If the string is just an integer
+        if (s.charAt(0) != '[') {
+            return new NestedInteger(Integer.parseInt(s));
+        }
+
+        // Create an empty list
+        NestedInteger result = new NestedInteger();
+
+        int num = 0;
+        int sign = 1;
+        boolean hasNum = false;
+
+        for (int i = 1; i < s.length() - 1; i++) {
+
+            char ch = s.charAt(i);
+
+            // Handle negative sign
+            if (ch == '-') {
+                sign = -1;
+            }
+
+            // Handle digits
+            else if (Character.isDigit(ch)) {
+                num = num * 10 + (ch - '0');
+                hasNum = true;
+            }
+
+            // When we find a comma
+            else if (ch == ',') {
+
+                if (hasNum) {
+                    result.add(new NestedInteger(sign * num));
+                    num = 0;
+                    sign = 1;
+                    hasNum = false;
+                }
+            }
+
+            // When we find a nested list
+            else if (ch == '[') {
+
+                int start = i;
+                int count = 1;
+
+                // Find the matching ']'
+                while (count != 0) {
+                    i++;
+
+                    if (s.charAt(i) == '[') {
+                        count++;
+                    } else if (s.charAt(i) == ']') {
+                        count--;
+                    }
+                }
+
+                // Recursively deserialize the nested list
+                result.add(deserialize(s.substring(start, i + 1)));
+
+                num = 0;
+                sign = 1;
+                hasNum = false;
+            }
+        }
+
+        // Add the last integer if present
+        if (hasNum) {
+            result.add(new NestedInteger(sign * num));
+        }
+
+        return result;
+    }
+}
