8a77f16809e3e0d26f6a4d3f2372ae6b2b4aef75
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-09-05 23:25:57 +0530

    Time: 2 ms (93.07%) | Memory: 43.2 MB (89.52%) - LeetSync

---
+++ 390-elimination-game/elimination-game.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int lastRemaining(int n) {
+        int head = 1;
+        int step = 1;
+        boolean left = true;
+
+        while (n > 1) {
+            // Head changes when eliminating from left
+            // or when eliminating from right with odd count
+            if (left || n % 2 == 1) {
+                head += step;
+            }
+
+            n /= 2;
+            step *= 2;
+            left = !left;
+        }
+
+        return head;
+    }
+}
