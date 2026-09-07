de7cdc7bd43e89b8d442188bd1380510cd511b63
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-09-07 22:49:50 +0530

    Time: 1 ms (84.83%) | Memory: 42.5 MB (93.54%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     public String decodeString(String s) {
         Stack<Integer> countStack = new Stack<>();
@@ -11,26 +9,21 @@ class Solution {
         for (char ch : s.toCharArray()) {
 
             if (Character.isDigit(ch)) {
-                // Build the repeat number
                 num = num * 10 + (ch - '0');
 
             } else if (ch == '[') {
-                // Save current string and repeat count
                 countStack.push(num);
                 stringStack.push(current);
 
-                // Reset for content inside brackets
+
                 num = 0;
                 current = new StringBuilder();
 
             } else if (ch == ']') {
-                // Get repeat count
+               
                 int repeat = countStack.pop();
 
-                // Get the string before '['
                 StringBuilder previous = stringStack.pop();
-
-                // Repeat current string
                 for (int i = 0; i < repeat; i++) {
                     previous.append(current);
                 }
@@ -38,7 +31,6 @@ class Solution {
                 current = previous;
 
             } else {
-                // Normal character
                 current.append(ch);
             }
         }
