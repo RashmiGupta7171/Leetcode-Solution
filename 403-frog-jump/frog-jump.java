47ecdeeef66fda2e285262a484431969e3f2e833
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-09-16 22:29:05 +0530

    Time: 46 ms (49.67%) | Memory: 50.4 MB (66.74%) - LeetSync

---
+++ 403-frog-jump/frog-jump.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+    public boolean canCross(int[] stones) {
+        int n = stones.length;
+
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        for (int stone : stones) {
+            map.put(stone, new HashSet<>());
+        }
+
+        // First jump must be 1 unit
+        map.get(0).add(0);
+
+        for (int stone : stones) {
+            for (int jump : map.get(stone)) {
+
+                for (int nextJump = jump - 1;
+                     nextJump <= jump + 1;
+                     nextJump++) {
+
+                    if (nextJump <= 0)
+                        continue;
+
+                    int nextStone = stone + nextJump;
+
+                    if (nextStone == stones[n - 1])
+                        return true;
+
+                    if (map.containsKey(nextStone)) {
+                        map.get(nextStone).add(nextJump);
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
