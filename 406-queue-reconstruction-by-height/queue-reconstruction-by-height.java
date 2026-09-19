8123957be4cfd26b222c7cbe147676c2322eaca0
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-09-19 23:08:23 +0530

    Time: 7 ms (93.44%) | Memory: 47.3 MB (65.81%) - LeetSync

---
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[][] reconstructQueue(int[][] people) {
+
+        // Sort by height descending.
+        // If heights are equal, sort k ascending.
+        java.util.Arrays.sort(people, (a, b) -> {
+            if (a[0] != b[0])
+                return b[0] - a[0];
+            return a[1] - b[1];
+        });
+
+        java.util.List<int[]> queue = new java.util.ArrayList<>();
+
+        // Insert each person at their k position
+        for (int[] person : people) {
+            queue.add(person[1], person);
+        }
+
+        return queue.toArray(new int[queue.size()][]);
+    }
+}
