65b3d80cb2078cd596f6f2bfb7cea94668d4cb02
Rashmi Gupta <rashmigupta.official3@gmail.com>
2026-09-20 18:31:53 +0530

    Time: 20 ms (58.66%) | Memory: 47.9 MB (67.77%) - LeetSync

---
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -0,0 +1,85 @@
+import java.util.*;
+
+class Solution {
+    public int trapRainWater(int[][] heightMap) {
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+
+        // If there are not enough rows/columns to trap water
+        if (m <= 2 || n <= 2) {
+            return 0;
+        }
+
+        // Min-heap: [height, row, column]
+        PriorityQueue<int[]> pq = new PriorityQueue<>(
+            (a, b) -> Integer.compare(a[0], b[0])
+        );
+
+        boolean[][] visited = new boolean[m][n];
+
+        // Add all boundary cells
+        for (int i = 0; i < m; i++) {
+            pq.offer(new int[]{heightMap[i][0], i, 0});
+            pq.offer(new int[]{heightMap[i][n - 1], i, n - 1});
+
+            visited[i][0] = true;
+            visited[i][n - 1] = true;
+        }
+
+        for (int j = 1; j < n - 1; j++) {
+            pq.offer(new int[]{heightMap[0][j], 0, j});
+            pq.offer(new int[]{heightMap[m - 1][j], m - 1, j});
+
+            visited[0][j] = true;
+            visited[m - 1][j] = true;
+        }
+
+        int water = 0;
+
+        // Four directions
+        int[][] directions = {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+        };
+
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+
+            int height = current[0];
+            int row = current[1];
+            int col = current[2];
+
+            for (int[] dir : directions) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                // Check boundaries and visited status
+                if (newRow < 0 || newRow >= m ||
+                    newCol < 0 || newCol >= n ||
+                    visited[newRow][newCol]) {
+                    continue;
+                }
+
+                visited[newRow][newCol] = true;
+
+                int neighborHeight = heightMap[newRow][newCol];
+
+                // Water trapped at this cell
+                if (neighborHeight < height) {
+                    water += height - neighborHeight;
+                }
+
+                // The effective boundary height
+                pq.offer(new int[]{
+                    Math.max(height, neighborHeight),
+                    newRow,
+                    newCol
+                });
+            }
+        }
+
+        return water;
+    }
+}
